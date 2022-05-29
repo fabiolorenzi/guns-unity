be79ddb0b1b903066af003db4ddb1d07c86dd029
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-05-29 07:55:38 +0100

    developed script for shooting

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -7,9 +7,49 @@ public class PlayerShoot : MonoBehaviour
     public float bpm;
     private float time = 0f;
 
-    public Camera fps_cam;
+    [SerializeField]
+    private bool isSingle = true;
+    [SerializeField]
+    private bool isAuto = false;
+
     private AudioSource audioController;
 
     [SerializeField]
     private AudioClip sound;
+
+    public void Start()
+    {
+        audioController = GetComponentInChildren<AudioSource>();
+    }
+
+    public void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            isSingle = !isSingle;
+            isAuto = !isAuto;
+        }
+
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        if(isSingle && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("single");
+            audioController.clip = sound;
+            audioController.Play();
+        }
+        else if(isAuto && Input.GetMouseButton(0))
+        {
+            if(Time.time >= time)
+            {
+                Debug.Log("auto");
+                audioController.clip = sound;
+                audioController.Play();
+                time += 60 / bpm;
+            }
+        }
+    }
 }
